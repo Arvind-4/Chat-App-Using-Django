c6a4b0716f70b138d477cd053cb7e272bcc5f796
Arvind-4 <lordarvindthegamer@gmail.com>
2023-12-11 16:46:55 +0530

    Added: chat app

---
+++ chat/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Room(models.Model):
+	name = models.CharField(max_length=100)
+
+class Messages(models.Model):
+	user  = models.CharField(max_length=1000000)
+	room  = models.CharField(max_length=1000000)
+	value = models.CharField(max_length=1000000)
+	date  = models.DateTimeField(default=timezone.now)
